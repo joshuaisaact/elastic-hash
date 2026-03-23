b48b0a3ea813c5b7d9185e7bef3c345f2c5ed296
Josh <joshuaisaact@gmail.com>
2026-03-23 01:05:49 +0000

    reduce MAX_PROBES from 32 to 24

--- src/hybrid.zig
+++ src/hybrid.zig
@@ -5,7 +5,7 @@ const std = @import("std");
 const math = std.math;
 
 pub const BUCKET_SIZE = 16;
-const MAX_PROBES = 100;
+const MAX_PROBES = 24;
 const TOMBSTONE: u8 = 0xFF;
 
 // Paper parameters
