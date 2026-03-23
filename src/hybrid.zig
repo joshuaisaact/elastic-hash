83d89ba5b2b6efc45282f9eff5549f443d222fb6
Josh <joshuaisaact@gmail.com>
2026-03-23 08:10:13 +0000

    Revert "two-round multiply hash for better fingerprint distribution"
    This reverts commit 7b36a0a4f1afeaf66817e46b26e867710bb8c4a9.

--- src/hybrid.zig
+++ src/hybrid.zig
@@ -395,9 +395,7 @@ pub const HybridElasticHash = struct {
     }
 
     inline fn hash(key: u64) u64 {
-        var h = key *% 0x517cc1b727220a95;
-        h ^= h >> 32;
-        h *%= 0x517cc1b727220a95;
+        const h = key *% 0x517cc1b727220a95;
         return h ^ (h >> 32);
     }
 
