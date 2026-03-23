33c9feb31cb593c1a5c86c251fb09d0f536a81d1
Josh <joshuaisaact@gmail.com>
2026-03-23 01:10:16 +0000

    Revert "reduce MAX_LOOKUP_TIERS from 8 to 6"
    This reverts commit d42e9ee45eab506ac0f8c2f3be5d194293a32f02.

--- src/hybrid.zig
+++ src/hybrid.zig
@@ -554,7 +554,7 @@ pub const HybridElasticHash = struct {
         }
     }
 
-    const MAX_LOOKUP_TIERS = 6;
+    const MAX_LOOKUP_TIERS = 8;
 
     pub fn get(self: *const Self, key: u64) ?u64 {
         const h = hash(key);
