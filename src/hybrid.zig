a355018e6e0df509f383bdc65fa2455893798d8c
Josh <joshuaisaact@gmail.com>
2026-03-23 08:36:44 +0000

    remove xor-shift from hash (just multiply)

--- src/hybrid.zig
+++ src/hybrid.zig
@@ -395,8 +395,7 @@ pub const HybridElasticHash = struct {
     }
 
     inline fn hash(key: u64) u64 {
-        const h = key *% 0x517cc1b727220a95;
-        return h ^ (h >> 32);
+        return key *% 0x517cc1b727220a95;
     }
 
     inline fn fingerprint(h: u64) u8 {
